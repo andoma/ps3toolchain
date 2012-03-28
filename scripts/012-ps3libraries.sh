e74ce5cbe4707adf1317be95337353673a1fcdf9
Andreas Öman <andreas@lonelycoder.com>
2012-03-28 23:05:21 +0200

    Pick correct version of ps3libraries

--- scripts/012-ps3libraries.sh
+++ scripts/012-ps3libraries.sh
@@ -2,7 +2,7 @@
 # ps3libraries.sh by Dan Peori (dan.peori@oopo.net)
 
 ## Download the source code.
-wget --continue --no-check-certificate https://github.com/ooPo/PS3LIBRARIES/tarball/master -O ps3libraries.tar.gz || { exit 1; }
+wget --continue --no-check-certificate https://github.com/andoma/ps3libraries/tarball/b4721540b3531e2a049f8979badf4a96df1f32f2 -O ps3libraries.tar.gz || { exit 1; }
 
 ## Unpack the source code.
 rm -Rf ps3libraries && mkdir ps3libraries && tar --strip-components=1 --directory=ps3libraries -xvzf ps3libraries.tar.gz && cd ps3libraries || { exit 1; }
