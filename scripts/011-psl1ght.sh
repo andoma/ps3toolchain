1493a5314319b6f0a9fee7fe5ff6ea7b5bf8f466
Andreas Öman <andreas@lonelycoder.com>
2012-03-28 23:25:58 +0200

    Correct PSL1GHT too

--- scripts/011-psl1ght.sh
+++ scripts/011-psl1ght.sh
@@ -2,7 +2,7 @@
 # psl1ght.sh by Dan Peori (dan.peori@oopo.net)
 
 ## Download the source code.
-wget --continue --no-check-certificate https://github.com/HACKERCHANNEL/PSL1GHT/tarball/master -O psl1ght.tar.gz || { exit 1; }
+wget --continue --no-check-certificate https://github.com/andoma/PSL1GHT/tarball/aa6ce4aa80d6e64c3c2256eca8521dd346874399 -O psl1ght.tar.gz || { exit 1; }
 
 ## Unpack the source code.
 rm -Rf psl1ght && mkdir psl1ght && tar --strip-components=1 --directory=psl1ght -xvzf psl1ght.tar.gz || { exit 1; }
