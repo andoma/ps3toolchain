7f0b49665b4816bd1bdfa748624a1e5dddb57bda
Andreas Öman <andreas@lonelycoder.com>
2012-03-31 19:30:50 +0200

    Compile without malloc. Showtime will use its own

--- scripts/003-newlib-1.19.0-PPU.sh
+++ scripts/003-newlib-1.19.0-PPU.sh
@@ -14,7 +14,7 @@ cat ../../patches/newlib-1.19.0-PS3.patch | patch -p1 || { exit 1; }
 mkdir build-ppu && cd build-ppu || { exit 1; }
 
 ## Configure the build.
-../configure --prefix="$PS3DEV/host/ppu" --target="ppu" \
+CFLAGS=-DMALLOC_PROVIDED=1 ../configure --prefix="$PS3DEV/host/ppu" --target="ppu" \
     --enable-newlib-multithread \
     --enable-newlib-hw-fp \
     || { exit 1; }
