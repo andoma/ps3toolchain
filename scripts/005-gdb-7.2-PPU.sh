3de139e5a78e0056e64f10cb3bc5023a189a4d4e
Andreas Öman <andreas@lonelycoder.com>
2012-04-19 12:39:20 +0200

    Fix GDB build

--- scripts/005-gdb-7.2-PPU.sh
+++ scripts/005-gdb-7.2-PPU.sh
@@ -5,7 +5,7 @@
 wget --continue ftp://ftp.gnu.org/gnu/gdb/gdb-7.2a.tar.bz2 || { exit 1; }
 
 ## Unpack the source code.
-rm -Rf gdb-7.2 && tar xfvj gdb-7.2.tar.bz2 && cd gdb-7.2 || { exit 1; }
+rm -Rf gdb-7.2 && tar xfvj gdb-7.2a.tar.bz2 && cd gdb-7.2 || { exit 1; }
 
 ## Patch the source code.
 cat ../../patches/gdb-7.2-PS3.patch | patch -p1 || { exit ; }
