5289bf8c6ce0be91606b80da4707c77c9c810de1
Andreas Öman <andreas@lonelycoder.com>
2012-04-29 20:34:48 +0200

    Drop the nomalloc libc - linker will fix this anyway for us. Pull PSL1GHT forward.

--- scripts/011-psl1ght.sh
+++ scripts/011-psl1ght.sh
@@ -2,7 +2,7 @@
 # psl1ght.sh by Dan Peori (dan.peori@oopo.net)
 
 ## Download the source code.
-wget --continue --no-check-certificate https://github.com/andoma/PSL1GHT/tarball/696cd9e09ec7c2fdf23916c52c42e15116ec7745 -O psl1ght.tar.gz || { exit 1; }
+wget --continue --no-check-certificate https://github.com/andoma/PSL1GHT/tarball/73556a46130d10911126393cdd49c3cfb2b246e8 -O psl1ght.tar.gz || { exit 1; }
 
 ## Unpack the source code.
 rm -Rf psl1ght && mkdir psl1ght && tar --strip-components=1 --directory=psl1ght -xvzf psl1ght.tar.gz || { exit 1; }
