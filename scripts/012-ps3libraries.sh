0bbc847895c3fd1dbec4a26e0eb59f0ae946e354
Andreas Öman <andreas@lonelycoder.com>
2012-05-04 12:41:37 +0200

    New ps3libraries

--- scripts/012-ps3libraries.sh
+++ scripts/012-ps3libraries.sh
@@ -2,7 +2,7 @@
 # ps3libraries.sh by Dan Peori (dan.peori@oopo.net)
 
 ## Download the source code.
-wget --continue --no-check-certificate https://github.com/andoma/ps3libraries/tarball/b4721540b3531e2a049f8979badf4a96df1f32f2 -O ps3libraries.tar.gz || { exit 1; }
+wget --continue --no-check-certificate https://github.com/andoma/ps3libraries/tarball/8f221c4558bdbe6f24eab1d955cd7b7626058d68 -O ps3libraries.tar.gz || { exit 1; }
 
 ## Unpack the source code.
 rm -Rf ps3libraries && mkdir ps3libraries && tar --strip-components=1 --directory=ps3libraries -xvzf ps3libraries.tar.gz && cd ps3libraries || { exit 1; }
