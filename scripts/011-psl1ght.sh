57a4c9e6a33e0d7cbb2873c954e42e9f791adbd1
Andreas Öman <andreas@lonelycoder.com>
2012-07-29 21:50:35 +0200

    New PSL1GHT

--- scripts/011-psl1ght.sh
+++ scripts/011-psl1ght.sh
@@ -2,7 +2,7 @@
 # psl1ght.sh by Dan Peori (dan.peori@oopo.net)
 
 ## Download the source code.
-wget --continue --no-check-certificate https://github.com/andoma/PSL1GHT/tarball/73556a46130d10911126393cdd49c3cfb2b246e8 -O psl1ght.tar.gz || { exit 1; }
+wget --continue --no-check-certificate https://github.com/andoma/PSL1GHT/tarball/4b48cae8c469a1ccfa2899aefc1a03fdd247a67e -O psl1ght.tar.gz || { exit 1; }
 
 ## Unpack the source code.
 rm -Rf psl1ght && mkdir psl1ght && tar --strip-components=1 --directory=psl1ght -xvzf psl1ght.tar.gz || { exit 1; }
